3abfe55064b80c704b4376e99a622d1c55230e1f
daniel_sobisz <s.daniel@wwg.it>
2021-11-28 19:34:40 +0100

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const path = require(`path`);
 
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://danielsobisz.github.io",
     title: "Sonia Kuras",
   },
   pathPrefix: "/sonia-kuras-new",
