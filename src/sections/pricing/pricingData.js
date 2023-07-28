69e1c8b74a7e4a8d23f11c5d0e8c4eaa18f10e9c
daniel_sobisz <s.daniel@wwg.it>
2023-07-28 10:20:16 +0200

    add new prices

--- src/sections/pricing/pricingData.js
+++ src/sections/pricing/pricingData.js
@@ -1,14 +1,14 @@
 export const pricesData = [
   {
     title: 'Psychoterapia indywidualna',
-    price: '170 zł / 50 min',
+    price: '200 zł / 50 min',
   },
   {
     title: 'Sesja Ruchu Autentycznego',
-    price: '170 zł / 50 min',
+    price: '200 zł / 50 min',
   },
   {
     title: 'Psychoterapia dla kobiet w ciąży i diady (matka-niemowlę)',
-    price: '200 zł / 60 min',
+    price: '300 zł / 75 min',
   },
 ];
