3042ced8413e2d879c35255287a9d1213a233707
danielsobisz <50883006+danielsobisz@users.noreply.github.com>
2025-11-03 22:15:06 +0100

    update content

--- src/sections/pricing/pricingData.js
+++ src/sections/pricing/pricingData.js
@@ -1,14 +1,18 @@
 export const pricesData = [
   {
     title: 'Psychoterapia indywidualna',
-    price: '200 zł / 50 min',
+    price: '230 zł / 50 min',
+  },
+  {
+    title: 'Psychoterapia grupowa',
+    price: '220 zł / 120 min',
   },
   {
     title: 'Sesja Ruchu Autentycznego',
-    price: '200 zł / 50 min',
+    price: '230 zł / 50 min',
   },
   {
     title: 'Psychoterapia dla kobiet w ciąży i diady (matka-niemowlę)',
-    price: '300 zł / 75 min',
+    price: '345 zł / 75 min',
   },
 ];
